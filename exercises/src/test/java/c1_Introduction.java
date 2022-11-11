296fc4fc698151e97de297723e93fafc2f633196
tushark-16bit <kalra.tushar.16@gmail.com>
2022-11-11 21:51:38 +0530

    Finished Chapter 1

--- exercises/src/test/java/c1_Introduction.java
+++ exercises/src/test/java/c1_Introduction.java
@@ -2,11 +2,14 @@ import org.junit.jupiter.api.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.time.Duration;
+import java.time.temporal.TemporalUnit;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.atomic.AtomicReference;
+import java.util.stream.Collectors;
 
 import static java.util.Collections.emptyList;
 import static org.junit.jupiter.api.Assertions.*;
@@ -20,6 +23,13 @@ import static org.junit.jupiter.api.Assertions.*;
  *
  * https://projectreactor.io/docs/core/release/reference/#intro-reactive
  * https://projectreactor.io/docs/core/release/reference/#reactive.subscribe
+ *
+ * By the act of subscribing, you tie the Publisher to a Subscriber,
+ * which triggers the flow of data in the whole chain.
+ * This is achieved internally by a single request signal
+ * from the Subscriber that is propagated upstream, all the way back to the
+ * source Publisher.
+ *
  * https://projectreactor.io/docs/core/release/reference/#_subscribe_method_examples
  *
  * Useful documentation:
@@ -41,7 +51,8 @@ public class c1_Introduction extends IntroductionBase {
     public void hello_world() {
         Mono<String> serviceResult = hello_world_service();
 
-        String result = null; //todo: change this line only
+//        String result = serviceResult.block();
+        String result = serviceResult.blockOptional().orElse("");
 
         assertEquals("Hello World!", result);
     }
@@ -55,8 +66,12 @@ public class c1_Introduction extends IntroductionBase {
         Exception exception = assertThrows(IllegalStateException.class, () -> {
             Mono<String> serviceResult = unresponsiveService();
 
-            String result = null; //todo: change this line only
+            String result =
+                    serviceResult.block(Duration.ofSeconds(1)); //todo:
+            // change this line
+            // only
         });
+        System.out.println(exception.getMessage());
 
         String expectedMessage = "Timeout on blocking read for 1";
         String actualMessage = exception.getMessage();
@@ -72,7 +87,9 @@ public class c1_Introduction extends IntroductionBase {
     public void empty_service() {
         Mono<String> serviceResult = emptyService();
 
-        Optional<String> optionalServiceResult = null; //todo: change this line only
+        Optional<String> optionalServiceResult = serviceResult.blockOptional(); //todo:
+        // change this
+        // line only
 
         assertTrue(optionalServiceResult.isEmpty());
         assertTrue(emptyServiceIsCalled.get());
@@ -89,7 +106,7 @@ public class c1_Introduction extends IntroductionBase {
     public void multi_result_service() {
         Flux<String> serviceResult = multiResultService();
 
-        String result = serviceResult.toString(); //todo: change this line only
+        String result = serviceResult.blockFirst(); //todo: change this line only
 
         assertEquals("valid result", result);
     }
@@ -103,7 +120,11 @@ public class c1_Introduction extends IntroductionBase {
     public void fortune_top_five() {
         Flux<String> serviceResult = fortuneTop5();
 
-        List<String> results = emptyList(); //todo: change this line only
+//        List<String> results = serviceResult.buffer().blockFirst(); //todo: change this
+        List<String> results = serviceResult.collect(Collectors.toList()).block();
+        //todo:
+        // change this
+        // line only
 
         assertEquals(Arrays.asList("Walmart", "Amazon", "Apple", "CVS Health", "UnitedHealth Group"), results);
         assertTrue(fortuneTop5ServiceIsCalled.get());
@@ -128,6 +149,8 @@ public class c1_Introduction extends IntroductionBase {
 
         serviceResult
                 .doOnNext(companyList::add)
+                .doOnNext(System.out::println)
+                .subscribe()
         //todo: add an operator here, don't use any blocking operator!
         ;
 
@@ -152,6 +175,11 @@ public class c1_Introduction extends IntroductionBase {
         CopyOnWriteArrayList<String> companyList = new CopyOnWriteArrayList<>();
 
         fortuneTop5()
+                .subscribe(
+                        companyList::add,
+                        null,
+                        () -> serviceCallCompleted.set(true)
+                )
         //todo: change this line only
         ;
 
